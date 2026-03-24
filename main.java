931739cf280c6c28cfc32247a8b2d9ea1cfbb054
Cleber Figueiredo <Cleber Nascimento>
2026-03-23 23:36:56 -0300

    feat.. lógica de classificação de media + interface

--- main.java
+++ main.java
@@ -1,1 +1,23 @@
+public class main {
+    public static String classificarMedia(double media) {
+        if (media < 0 || media > 10) {
+            return "Média inválida";
+        } else if (media >= 7) {
+            return "Aprovado";
+        } else if (media >= 5) {
+            return "Recuperação";
+        } else {
+            return "Reprovado";
+        }
+    }
 
+
+        public static void main(String[] args) {
+            double mediaAluno = 6.5;
+
+            String resultado = classificarMedia(mediaAluno);
+
+            System.out.println("Média final: " + mediaAluno);
+            System.out.println("Classificação: " + resultado);
+        }
+    }

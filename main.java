67dcee1e1e8f843e34d8eb9007f789ad5a14f63a
Raphael_Issa <raphaelbiof0@gmail.com>
2026-03-24 02:10:17 -0300

    feat-Colocando logica + teste

--- main.java
+++ main.java
@@ -1,1 +1,41 @@
+public class Main{
+    
+    public static double calcular_media(double[] notas){
+        
+        
+        double mediaAluno = 0;
+        double soma = 0;
 
+        for (int i = 0; i < notas.length; i++){
+
+            soma += notas[i];
+
+        }//fim for que soma as notas recebidas
+
+        mediaAluno = soma/notas.length;
+        //calculo da media
+    
+        return mediaAluno;
+
+
+}//fim função de calculo
+
+
+
+public static void main(String[] args) {
+    
+    double[] n = {7,8,9,4,1,5,8}; 
+
+    double mediaAluno = calcular_media(n);
+
+    System.out.println("A media é: " + mediaAluno); //teste, logo sera removido
+
+
+}//fim main
+
+
+
+
+
+
+}//fim class

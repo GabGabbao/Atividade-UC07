f88cecbfdffc218c724641e4c32e38e9d4a617f7
Leonardo-pr <leonardosoaresaraujo92@gmail.com>
2026-03-25 01:15:45 -0300

    segunda versão

--- main.java
+++ main.java
@@ -1,9 +1,29 @@
+package main;
 
 import java.util.Scanner;
 
-public class main {
+public class Main {
 
-	public static void main(String[] args) { 
-		Scanner ler = new Scanner(System.in);
-        System.out.print("oi")
-	}
+    public static void main(String[] args) {
+
+        Scanner ler = new Scanner(System.in);
+        
+        int quantidadeNotas;
+
+        System.out.print("Digite a quantidade de notas: ");
+        quantidadeNotas = ler.nextInt();
+
+        double[] notas = new double[quantidadeNotas];
+
+        for (int i = 0; i < quantidadeNotas; i++) {
+            System.out.print("Digite a nota " + (i + 1) + ": ");
+            notas[i] = (int) ler.nextDouble();
+        }
+
+        System.out.println("Notas digitadas:");
+        for (int i = 0; i < quantidadeNotas; i++) {
+            System.out.println(notas[i]);
+        }
+
+    }
+}

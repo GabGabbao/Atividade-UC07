9b4bb20129916cb4243c722510dbb8c2bf934d1e
R. Castor <rafaelcastorguarnieri@gmail.com>
2026-03-25 06:20:29 -0300

    Adicionada validação de entrada.

--- main.java
+++ main.java
@@ -7,23 +7,49 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner ler = new Scanner(System.in);
+
+        int quantidadeNotas = 0;
         
-        int quantidadeNotas;
-
-        System.out.print("Digite a quantidade de notas: ");
-        quantidadeNotas = ler.nextInt();
-
-        double[] notas = new double[quantidadeNotas];
-
-        for (int i = 0; i < quantidadeNotas; i++) {
-            System.out.print("Digite a nota " + (i + 1) + ": ");
-            notas[i] = (int) ler.nextDouble();
+        while (true) {
+            System.out.print("Digite a quantidade de notas: ");
+            if (ler.hasNextInt()) {
+                quantidadeNotas = ler.nextInt();
+                if (quantidadeNotas > 0) {
+                    break;
+                } else {
+                    System.out.println("Deve haver >0 notas.");
+                }
+            } else {
+                System.out.println("Digite um número válido.");
+                ler.next();
+            }
         }
 
-        System.out.println("Notas digitadas:");
+        double[] notas = new double[quantidadeNotas];
+        
+        for (int i = 0; i < quantidadeNotas; i++) {
+            while (true) {
+                System.out.print("Digite a nota " + (i + 1) + ": ");
+                if (ler.hasNextDouble()) {
+                    double nota = ler.nextDouble();
+                    if (nota >= 0 && nota <= 10) {
+                        notas[i] = nota;
+                        break;
+                    } else {
+                        System.out.println("Notas devem estar entre 0 e 10");
+                    }
+                } else {
+                    System.out.println("Digite um valor numérico válido.");
+                    ler.next();
+                }
+            }
+        }
+
+        System.out.println("\nNotas digitadas:");
         for (int i = 0; i < quantidadeNotas; i++) {
             System.out.println(notas[i]);
         }
 
+        ler.close();
     }
 }
